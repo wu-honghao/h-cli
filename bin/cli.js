1be543fa087ce1d612c01b397ce54d868c9d4751
unknown <2698981813@qq.com>
2022-10-17 20:21:53 +0800

    feat: 下载远程仓库模板代码

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,10 @@
 #! /usr/bin/env node
 // console.log(process.argv);
-const { program } = require('commander');
-const myhelp = require('../lib/core/help');
-const mycommander = require('../lib/core/myCommand');
+import { program } from 'commander';
+import { myHelp } from '../lib/core/help.js';
+import { myCommander } from '../lib/core/myCommand.js';
 
-myhelp(program);
-mycommander(program);
+myHelp(program);
+myCommander(program);
 
 program.parse(process.argv);
---
+++ config.js
@@ -0,0 +1,8 @@
+export default {
+    framwork: ['express', 'koa', 'egg'],
+    framworkUrl: {
+        egg: 'https://gitee.com/beiyaoyaoyao/egg-template.git',
+        express: 'https://gitee.com/beiyaoyaoyao/express-template.git',
+        koa: 'https://gitee.com/beiyaoyaoyao/koa-template.git',
+    },
+};
