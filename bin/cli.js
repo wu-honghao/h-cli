a32fd80358b10a32bf72fe4c13ab3e02f1cb7701
unknown <2698981813@qq.com>
2022-10-17 17:43:00 +0800

    feat: 逻辑代码模块化拆分

--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,10 @@
 #! /usr/bin/env node
 // console.log(process.argv);
 const { program } = require('commander');
+const myhelp = require('../lib/core/help');
+const mycommander = require('../lib/core/myCommand');
 
-program.option('-f --framework <framework>', '设置框架');
-program
-    .command('create <project> [other...]')
-    .alias('crt')
-    .description('创建项目')
-    .action((project, args) => {
-        console.log(project);
-        console.log(args);
-    });
+myhelp(program);
+mycommander(program);
+
 program.parse(process.argv);
