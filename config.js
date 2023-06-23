03741b0b6933cc8ae0a5d2c2787bec76c164f88a
wu-honghao <2698981813@qq.com>
2023-06-23 22:46:27 +0800

    feat: 更新 express 脚手架

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export default {
     framwork: ['express', 'koa', 'egg'],
     framworkUrl: {
         egg: 'https://gitee.com/beiyaoyaoyao/egg-template.git',
-        express: 'https://gitee.com/beiyaoyaoyao/express-template.git',
+        express: 'https://github.com/wu-honghao/express-basic.git',
         koa: 'https://gitee.com/beiyaoyaoyao/koa-template.git',
     },
 };
